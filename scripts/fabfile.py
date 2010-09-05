1554e0ed786febbbe20b150d732d8d5f603c1470
Dakshina Murthy <dkmurthy9@gmail.com>
2010-09-05 18:03:39 +0530

    Cleanup of fabfile.py

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -1,18 +1,30 @@
-
 from pysis.scripts import bootstrap_django
 bootstrap_django.main()
 
 import os
 import sys
-from fabric.api import env, local, run
+import fabric
+from fabric.api import env
 from django.conf import settings
 
 env.hosts = [os.environ.get('MYSERVER')]
 
-# use test settings, not actual settings
-os.environ['DJANGO_SETTINGS_MODULE'] = 'pysis.settings.test_settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pysis.settings.settings'
 os.environ['PYTHONPATH'] = '.:..:%s/apps' %  settings.PROJECT_ROOT
 
+env.master_repo = 'ssh://hg@bitbucket.org/dkmurthy/pysis'
+env.remote_repo = '/var/www/pysis'
+env.remote_env = '~/virtualenvs/pysis/bin/'
+
+def local(cmd):
+    fabric.api.local(cmd, capture=False)
+
+def run(cmd):
+    env.cmd = cmd
+    fabric.api.run('source %(remote_env)s/activate && '
+                   'cd %(remote_repo)s && '
+                   '%(cmd)s' % env
+                  )
 
 def run_pylint():
     local('pylint ' +
@@ -23,35 +35,46 @@ def run_pylint():
           '-d E1101 ' +
           '--reports=n ' +
           '--disable=C,R,W ' +
-          settings.PROJECT_ROOT, capture=False)
+          settings.PROJECT_ROOT)
 
 
 def run_nose():
+    # use test settings, not actual settings
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'pysis.settings.test_settings'
+
     local('nosetests -v -d -x ' +
            '--with-doctest ' +
            '--with-django ' +
            '--with-djangoliveserver ' +
-           '--with-selenium ', capture=False)
+           '--with-selenium ')
 
 def test():
     run_pylint()
     run_nose()
 
+def test_on_server():
+    run('fab -f scripts/fabfile.py test')
+
+def push():
+    local('hg push %(master_repo)s' % env)
+    run('hg fetch %(master_repo)s' % env)
+
+def install_requirements():
+    run('pip install -r requirements/requirements.txt -q')
+
+def upgrade_db():
+    run('python manage.py syncdb')
+    run('python manage.py migrate accounts')
+
+def restart_webserver():
+    run('scripts/restart_webserver.sh')
+
 def deploy(skip_tests='no'):
     if skip_tests != 'yes':
         test()
 
-    master_repo = 'ssh://hg@bitbucket.org/dkmurthy/pysis'
-    remote_repo = '/var/www/pysis'
-    remote_env = '~/virtualenvs/pysis/bin/'
-
-    local('hg push %s' % master_repo, capture=False)
-
-    run('hg fetch -R %s %s' % (remote_repo, master_repo))
-    run('source %s/activate && pip install -r %s/requirements/requirements.txt -q' % (remote_env, remote_repo))
-    run('%s/python %s/manage.py migrate accounts' % (remote_env, remote_repo))
-    run('source %s/activate && cd %s && fab -f %s/scripts/fabfile.py test ' % (remote_env, remote_repo, remote_repo))
-    run('source %s/activate && %s/scripts/restart_webserver.sh' % (remote_env, remote_repo))
-
-
-
+    push()
+    upgrade_db()
+    install_requirements()
+    restart_webserver()
+    test_on_server()
--- version.py
+++ version.py
@@ -1,2 +1,2 @@
-version = '75'
-last_modified_timestamp = '2010-09-05 15:42:16 IST'
+version = '76'
+last_modified_timestamp = '2010-09-05 18:03:39 IST'
